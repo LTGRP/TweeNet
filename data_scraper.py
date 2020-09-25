a5d60530a9be666aa72d6fc76d9c5742e4360493
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2020-09-24 22:44:17 -0500

    Add click to scraper and update scraper

--- data_scraper.py
+++ data_scraper.py
@@ -1,22 +1,7 @@
-import sys
-import time
 import json
 import tweepy
+import click
 from keys import key
-
-API_KEY = key['api_key']
-API_SECRET = key['api_secret']
-ACCESS_TOKEN = key['access_token']
-ACCESS_TOKEN_SECRET = key['access_token_secret']
-
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True,
-                 wait_on_rate_limit_notify=True,
-                 compression=True)
-
-target = "SpaceX"  # target account screen name
-target_info = api.get_user(screen_name=target)
 
 
 def connection_handler(cursor):
@@ -41,13 +26,37 @@ def lookup_friend_list(api, id):
     return adjacency_list
 
 
-result = {}
-result[target] = lookup_friend_list(api, target_info.id)
+@click.command()
+@click.argument('target')
+@click.option('-f', '--filename',
+              default='data.json',
+              show_default=True,
+              help='filename of output data')
+def fetch_data(target, filename):
+    auth = tweepy.OAuthHandler(key['api_key'], key['api_secret'])
+    auth.set_access_token(key['access_token'], key['access_token_secret'])
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True,
+                     compression=True)
+    try:
+        target_info = api.get_user(screen_name=target)
+    except tweepy.TweepError as e:
+        print(e)
+        exit()
 
-total = len(result[target])
-for i, user in enumerate(result[target]):
-    print("Looking up " + user["screen_name"] + "'s friends (" + str(i+1) + "/" + str(total) + ")")
-    result[user["screen_name"]] = lookup_friend_list(api, user["id"])
+    result = {}
+    print('Fetching target\'s friend list...' )
+    result[target] = lookup_friend_list(api, target_info.id)
+    total = len(result[target])
+    print(str(total) + " friends finded.")
 
-with open('data.json', 'w') as f:
-    json.dump(result, f, indent=4)
+    for i, user in enumerate(result[target]):
+        print("Looking up " + user["screen_name"] + "'s friends (" + str(i+1) + "/" + str(total) + ")")
+        result[user["screen_name"]] = lookup_friend_list(api, user["id"])
+
+    with open(filename, 'w') as f:
+        json.dump(result, f, indent=4)
+
+
+if __name__ == '__main__':
+    fetch_data()
